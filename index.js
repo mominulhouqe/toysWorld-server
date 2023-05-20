538b46aace372e024ab9e1731a3af487e569543d
mominul <ulhouqemomin39@gmail.com>
2023-05-20 20:21:01 +0600

    updated formating

--- index.js
+++ index.js
@@ -31,177 +31,56 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    // const addToyCollection = client.db("addedToys").collection("addedToys");
+    const addToyCollection = client.db("addedToys").collection("addedToys");
 
-    // // my Toys list
-    // app.get("/addToys", async (req, res) => {
-    //   let query = {};
-    //   if (req.query?.email) {
-    //     query = { sellerEmail: req.query.email };
-    //   }
-    //   const result = await addToyCollection.find(query).toArray();
-    //   res.send(result);
-    // });
+    // Get all toys or filter by seller email
+    app.get("/addToys", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { sellerEmail: req.query.email };
+      }
+      const result = await addToyCollection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .toArray();
+      res.send(result);
+    });
 
-    // // send data from db to client
-    // app.get("/addToys", async (req, res) => {
-    //   const cursor = addToyCollection.find().limit(20);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
+    
+    // Insert a new toy
+    app.post("/addToys", async (req, res) => {
+      const addedToy = req.body;
+      addedToy.createdAt = new Date();
+      const result = await addToyCollection.insertOne(addedToy);
+      res.send(result);
+    });
 
 
-// ...
-
-const addToyCollection = client.db("addedToys").collection("addedToys");
-
-// Get all toys or filter by seller email
-app.get("/addToys", async (req, res) => {
-  let query = {};
-  if (req.query?.email) {
-    query = { sellerEmail: req.query.email };
-  }
-  const result = await addToyCollection.find(query).sort({createdAt : -1}).toArray();
-  res.send(result);
-});
-
-// Insert a new toy
-app.post("/addToys", async (req, res) => {
-  const addedToy = req.body;
-  addedToy.createdAt = new Date();
-  const result = await addToyCollection.insertOne(addedToy);
-  res.send(result);
-});
-
-// Update a toy by ID
-// app.patch("/addToys/:id", async (req, res) => {
-//   const id = req.params.id;
-//   const updatedToy = req.body;
-//   const filter = { _id: ObjectId(id) };
-//   const updateDoc = {
-//     $set: updatedToy
-//   };
-//   const result = await addToyCollection.updateOne(filter, updateDoc);
-//   res.send(result);
-// });
-
-// Delete a toy by ID
-app.delete("/addToys/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: ObjectId(id) };
-  const result = await addToyCollection.deleteOne(query);
-  res.send(result);
-});
-
-// Search from db
-const indexKeys = {name:1};
-const indexOptions= {names:"toyZone"};
-const result = await addToyCollection.createIndex(indexKeys,indexOptions);
-app.get('/searchByToyName/:text', async (req, res) => {
-  const searchText = req.params.text;
-  const result = await addToyCollection.find({
-    name: { $regex: searchText, $options: "i" }
-  }).toArray();
-  res.send(result);
-});
+    // Delete a toy by ID
+    app.delete("/addToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await addToyCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
 
 
 
-
-    // // getData from input field and insert db
-    // app.post("/addToys", async (req, res) => {
-    //   const addedToys = req.body;
-
-    //   const result = await addToyCollection.insertOne(addedToys);
-    //   res.send(result);
-    // });
-    // // Updated From database
-    // app.patch("/addToys/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatedToy = req.body;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const options = {upsert: true}
-    //   console.log(updatedToy);
-    //   const updatedDoc = {
-    //     $set: {
-    //       name: updatingUser.name,
-    //       email: updatingUser.email,
-    //     },
-    //   };
-    //   const result = await addToyCollection.updateOne(filter, updatedDoc, options);
-    //   res.send(result);
-    // });
-
-    // // Delete From Database
-    // app.delete("/addToys/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await addToyCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-
-    // const serviceCollection = client.db('carDoctor').collection('services');
-    // const bookingCollection = client.db('carDoctor').collection('bookings');
-
-    // app.get('/services', async (req, res) => {
-    //     const cursor = serviceCollection.find();
-    //     const result = await cursor.toArray();
-    //     res.send(result);
-    // })
-
-    // app.get('/services/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = { _id: new ObjectId(id) }
-
-    //     const options = {
-    //         // Include only the `title` and `imdb` fields in the returned document
-    //         projection: { title: 1, price: 1, service_id: 1, img: 1 },
-    //     };
-
-    //     const result = await serviceCollection.findOne(query, options);
-    //     res.send(result);
-    // })
-
-    // // bookings
-    // app.get('/bookings', async (req, res) => {
-    //     console.log(req.query.email);
-    //     let query = {};
-    //     if (req.query?.email) {
-    //         query = { email: req.query.email }
-    //     }
-    //     const result = await bookingCollection.find(query).toArray();
-    //     res.send(result);
-    // })
-
-    // app.post('/bookings', async (req, res) => {
-    //     const booking = req.body;
-    //     console.log(booking);
-    //     const result = await bookingCollection.insertOne(booking);
-    //     res.send(result);
-    // });
-
-    // app.patch('/bookings/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const filter = { _id: new ObjectId(id) };
-    //     const updatedBooking = req.body;
-    //     console.log(updatedBooking);
-    //     const updateDoc = {
-    //         $set: {
-    //             status: updatedBooking.status
-    //         },
-    //     };
-    //     const result = await bookingCollection.updateOne(filter, updateDoc);
-    //     res.send(result);
-    // })
-
-    // app.delete('/bookings/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = { _id: new ObjectId(id) }
-    //     const result = await bookingCollection.deleteOne(query);
-    //     res.send(result);
-    // })
+    // Search from db
+    const indexKeys = { name: 1 };
+    const indexOptions = { names: "toyZone" };
+    const result = await addToyCollection.createIndex(indexKeys, indexOptions);
+    app.get("/searchByToyName/:text", async (req, res) => {
+      const searchText = req.params.text;
+      const result = await addToyCollection
+        .find({
+          name: { $regex: searchText, $options: "i" },
+        })
+        .toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
