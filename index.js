51d91ba8f4f56cab13f3cc245be07fcf56b0474e
mominul <ulhouqemomin39@gmail.com>
2023-05-21 11:36:56 +0600

    added sorting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+
 require("dotenv").config();
 
 //midleware
@@ -29,21 +30,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const addToyCollection = client.db("addedToys").collection("addedToys");
-    const addInsertGelleryCollection = client.db('insertToys').collection("insertToys")
+    const addInsertGelleryCollection = client
+      .db("insertToys")
+      .collection("insertToys");
 
+    // Inserted Gallary section
 
-    // Inserted Gallary section 
-    
     app.get("/insertToys", async (req, res) => {
       const cursor = addInsertGelleryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-  })
-
-
+    });
 
     // Get all toys or filter by seller email
     app.get("/addToys", async (req, res) => {
@@ -53,15 +53,24 @@ async function run() {
       }
       const result = await addToyCollection
         .find(query)
-        .sort({ price: -1 })
+        .sort({ price: 1 })
         .toArray();
-      res.send(result);
+
+      // Convert the "price" field to a numeric type for correct sorting
+      const sortedResult = result.map((toy) => ({
+        ...toy,
+        price: parseFloat(toy.price),
+      }));
+
+      // Sort the array based on the numeric "price" field
+      sortedResult.sort((a, b) => a.price - b.price);
+
+      res.send(sortedResult);
     });
 
     // Insert a new toy
     app.post("/addToys", async (req, res) => {
       const addedToy = req.body;
-
       const result = await addToyCollection.insertOne(addedToy);
       res.send(result);
     });
@@ -87,22 +96,6 @@ async function run() {
         .toArray();
       res.send(result);
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
